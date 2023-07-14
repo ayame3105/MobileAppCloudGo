2e50f56d89c859d25f25a69ca35cf80493f068e5
Vo Hoang Phuc <phucvo98031@gmail.com>
2023-07-14 15:35:07 +0700

    14/7 update them giao dien

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:first_layout/screens/dang_nhap_screen/dang_nhap_screen.dart';
 import 'package:first_layout/screens/home_screen/home_screen.dart';
 import 'package:first_layout/screens/lien_he_screen/lien_he_screen.dart';
-import 'package:first_layout/screens/main_page/main_page.dart';
-import 'package:first_layout/screens/test/test%202.dart';
+import 'package:first_layout/screens/main_page/bottom_navigation_bar.dart';
+import 'package:first_layout/screens/profile/HoSoCaNhan/profile.dart';
+import 'package:first_layout/screens/profile/menu_account.dart';
 import 'package:first_layout/screens/test/test_screen.dart';
 import 'package:first_layout/screens/tin_tuc_screen/tin_tuc_screen.dart';
 import 'package:flutter/material.dart';
@@ -21,12 +22,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Test(),
+      home: const DangNhap(),
     );
   }
 }
-
--- lib/screens/home_screen/home_screen.dart
+++ lib/screens/home_screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:first_layout/screens/profile/menu_account.dart';
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -8,124 +10,126 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-final List<String> imageList = [
-  'assets/images/1.png',
-  'assets/images/2.png',
-  'assets/images/3.png',
-  'assets/images/4.png'
-];
-
-final List<String> gridImageList = [
-  'assets/images/bag.png',
-  'assets/images/call.png',
-  'assets/images/care.png',
-  'assets/images/camera.png',
-  'assets/images/expand.png',
-  'assets/images/sales.png'
-];
-
-final List<String> gridTextList = [
-  'CloudSALES',
-  'CloudWORK',
-  'CloudCAM',
-  'CloudCARE',
-  'CloudCALL',
-  'Xem thêm'
-];
-
 class _HomeScreenState extends State<HomeScreen> {
+  int activeIndex = 0;
   @override
   Widget build(BuildContext context) {
+    final maxWidth = MediaQuery.of(context).size.width;
+    final maxHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Column(
-        children: [
-          // Container(
-          //   decoration: BoxDecoration(
-          //     color: Colors.blue,
-          //     borderRadius: BorderRadius.circular(16.0),
-          //   ),
-          //   child: Row(
-          //     children: [
-          //       SizedBox(
-          //         width: 50,
-          //         height: 50,
-          //         child: Image.asset('assets/images/user_ic.png'),
-          //       ),
-          //       const SizedBox(width: 10),
-          //       const Padding(
-          //         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
-          //         child: Column(
-          //           crossAxisAlignment: CrossAxisAlignment.start,
-          //           children: [
-          //             Text('Buổi sáng tốt lành'),
-          //             Text(
-          //               'NGUYỄN VĂN TOÀN',
-          //               style: TextStyle(fontWeight: FontWeight.bold),
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //     ],
-          //   ),
-          // ),
-          // CarouselSlider(
-          //   items: [
-          //     SizedBox(
-          //       width: 400,
-          //       height: 500,
-          //       child: Card(
-          //         shape: RoundedRectangleBorder(
-          //           borderRadius: BorderRadius.circular(20.0),
-          //         ),
-          //         child: Row(
-          //           children: [
-          //             SizedBox(
-          //               height: 100,
-          //               width: 100,
-          //               child: Image.asset(
-          //                 'assets/images/1.png',
-          //                 fit: BoxFit.cover,
-          //               ),
-          //             ),
-          //             const Padding(
-          //               padding: EdgeInsets.symmetric(horizontal: 20),
-          //               child: Column(
-          //                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          //                 crossAxisAlignment: CrossAxisAlignment.end,
-          //                 children: [
-          //                   Row(
-          //                     children: [
-          //                       ImageIcon(
-          //                         AssetImage('assets/images/sales.png'),
-          //                       ),
-          //                       Text('  CloudSALES'),
-          //                     ],
-          //                   ),
-          //                 ],
-          //               ),
-          //             ),
-          //           ],
-          //         ),
-          //       ),
-          //     ),
-          //   ],
-          //   options: CarouselOptions(
-          //     enlargeCenterPage: true,
-          //     autoPlay: true,
-          //     enableInfiniteScroll: true,
-          //   ),
-          // ),
-          GridView.builder(
+        body: SingleChildScrollView(
+          child: Column(children: [
+      SizedBox(
+          height: 300,
+          child: Stack(
+            children: [
+              Container(
+                height: maxHeight * 0.2,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20)),
+                ),
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const MenuAccount()),
+                    );
+                  },
+                  child: Row(
+                    children: [
+                      Container(
+                        child: SizedBox(
+                          width: maxWidth * 0.125,
+                          height: 50,
+                          child: Image.asset('assets/images/user_ic.png'),
+                        ),
+                      ),
+                      const Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 60),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text('Buổi sáng tốt lành'),
+                            Text(
+                              'NGUYỄN VĂN TOÀN',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 100,
+                left: 0,
+                right: 0,
+                child: Column(
+                  children: [
+                    CarouselSlider(
+                      items: [
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              'assets/images/crm.png',
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                        Container(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(20),
+                            child: Image.asset(
+                              'assets/images/crm_la_gi.png',
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ),
+                      ],
+                      options: CarouselOptions(
+                        enlargeCenterPage: true,
+                        autoPlay: true,
+                        enableInfiniteScroll: true,
+                        onPageChanged: (index, reason) {
+                          setState(() {
+                            activeIndex = index;
+                          });
+                        },
+                      ),
+                    ),
+                    AnimatedSmoothIndicator(
+                        activeIndex: activeIndex,
+                        count: 2)
+                  ],
+                ),
+
+              ),
+
+            ],
+          ),
+      ),
+      const SizedBox(height: 10),
+      SizedBox(
+          height: maxHeight * 0.3,
+          child: GridView.builder(
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 3,
-              crossAxisSpacing: 50,
-              mainAxisSpacing: 30,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
             ),
             itemCount: gridImageList.length,
             itemBuilder: (BuildContext context, int index) {
               return Column(
                 children: [
                   ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
                     child: Image.asset(
                       gridImageList[index],
                       width: 50,
@@ -144,59 +148,62 @@ class _HomeScreenState extends State<HomeScreen> {
               );
             },
           ),
-          const SizedBox(
-            height: 100,
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    'Tin tức',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    'Tất cả',
-                    style: TextStyle(color: Colors.blue),
-                  ),
-                ],
+      ),
+      SizedBox(
+          height: maxHeight * 0.07,
+          child: const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Tin tức',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
+              Text(
+                'Tất cả',
+                style: TextStyle(color: Colors.blue),
+              ),
+            ],
           ),
-          SizedBox(
-            width: 100,
-            child: ListView.builder(
-              scrollDirection: Axis.horizontal,
-              itemCount: listViewData.length,
-              itemBuilder: (BuildContext context, int index) {
-                final item = listViewData[index];
-                return Column(
-                  children: [
-                    Image.asset(
+      ),
+      SizedBox(
+          width: maxWidth,
+          height: 300,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: listViewData.length,
+            itemBuilder: (BuildContext context, int index) {
+              final item = listViewData[index];
+              return Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.asset(
                       item.imagePath,
                       width: 300,
                       height: 100,
                       fit: BoxFit.cover,
                     ),
-                    const SizedBox(height: 10),
-                    Flexible(
-                      child: Text(
-                        item.title,
-                        softWrap: true,
-                        textAlign: TextAlign.center,
-                      ),
+                  ),
+                  const SizedBox(height: 10),
+                  Flexible(
+                    child: Text(
+                      item.title,
+                      softWrap: true,
+                      textAlign: TextAlign.left,
                     ),
-                  ],
-                );
-              },
-            ),
+                  ),
+                ],
+              );
+            },
           ),
-        ],
       ),
+    ]),
+        )
     );
   }
+
 }
 
 class ListViewData {
@@ -222,4 +229,29 @@ final List<ListViewData> listViewData = [
     imagePath: 'assets/images/crm_la_gi.png',
     title: 'CRM là gì? Áp dụng chiến thuật CRM vào công ty',
   ),
+];
+
+final List<String> imageList = [
+  'assets/images/1.png',
+  'assets/images/2.png',
+  'assets/images/3.png',
+  'assets/images/4.png'
+];
+
+final List<String> gridImageList = [
+  'assets/images/sales.png',
+  'assets/images/bag.png',
+  'assets/images/camera.png',
+  'assets/images/care.png',
+  'assets/images/call.png',
+  'assets/images/expand.png',
+];
+
+final List<String> gridTextList = [
+  'CloudSALES',
+  'CloudWORK',
+  'CloudCAM',
+  'CloudCARE',
+  'CloudCALL',
+  'Xem thêm'
 ];
