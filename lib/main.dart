346da3f6f127c02687e1c3196dd0761dc5db7934
Vo Hoang Phuc <phucvo98031@gmail.com>
2023-07-14 16:44:01 +0700

    14/7 update them giao dien

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:first_layout/blocs/user_info_bloc/user_info_bloc.dart';
 import 'package:first_layout/screens/dang_nhap_screen/dang_nhap_screen.dart';
 import 'package:first_layout/screens/home_screen/home_screen.dart';
 import 'package:first_layout/screens/lien_he_screen/lien_he_screen.dart';
@@ -7,7 +8,7 @@ import 'package:first_layout/screens/profile/menu_account.dart';
 import 'package:first_layout/screens/test/test_screen.dart';
 import 'package:first_layout/screens/tin_tuc_screen/tin_tuc_screen.dart';
 import 'package:flutter/material.dart';
-
+import "package:flutter_bloc/flutter_bloc.dart";
 void main() {
   runApp(const MyApp());
 }
@@ -17,15 +18,33 @@ class MyApp extends StatelessWidget {
 
   // This widget is the root of your application.
   @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     title: 'Flutter Demo',
+  //     theme: ThemeData(
+  //       colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+  //       useMaterial3: true,
+  //     ),
+  //     home: const DangNhap(),
+  //   );
+  // }
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const DangNhap(),
-    );
+    return MultiBlocProvider(
+        providers:
+        [
+          BlocProvider(
+              create: (BuildContext context) => UserInfoBloc()
+          ),
+        ],
+        child:    MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const DangNhap(),
+      ));
   }
 }
