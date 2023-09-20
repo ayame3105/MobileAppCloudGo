119e77249c146bd4cf47b7b291f9c49ffc9516ca
Vo Hoang Phuc <phucvo98031@gmail.com>
2023-09-20 17:30:07 +0700

    chieu 20/9

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:first_layout/blocs/lead_info_bloc/lead_info_bloc.dart';
 import 'package:first_layout/blocs/user_info_bloc/user_info_bloc.dart';
 import 'package:first_layout/screens/CLoudWORK/cloud_work.dart';
+import 'package:first_layout/screens/CloudCALL/danh_ba/danh_ba.dart';
 import 'package:first_layout/screens/CloudGO/dang_nhap_screen/dang_nhap_screen.dart';
 import 'package:first_layout/screens/CloudGO/home_screen/home_screen.dart';
 import 'package:first_layout/screens/CloudGO/splash_screen/hello.dart';
 import 'package:first_layout/screens/CloudGO/splash_screen/splash_screen.dart';
+import 'package:first_layout/screens/CloudSALES/screens/home/calendar/danh_sach.dart';
 import 'package:first_layout/screens/CloudSALES/screens/home/home.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -44,12 +46,12 @@ class MyApp extends StatelessWidget {
         child:
         MaterialApp(
         debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
+        title: 'CloudGO',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Hello(),
+        home: Calendar(),
       ));
   }
 }
